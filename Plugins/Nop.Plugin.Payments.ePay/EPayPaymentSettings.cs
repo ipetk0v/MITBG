3ab94f7a4a9cb8865473e5c1704d66f0847b7e98
Tsaplinov Evgeny <tcaplinov.evgeny@gmail.com>
2019-08-01 22:53:46 +0300

    Add epay plugin

---
+++ Plugins/Nop.Plugin.Payments.ePay/EPayPaymentSettings.cs
@@ -0,0 +1,29 @@
+using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Payments.ePay
+{
+    public class EPayPaymentSettings : ISettings
+    {
+        public bool UseSandbox { get; set; }
+
+        public string CustomerNumber { get; set; }
+
+        public string DealerEmail { get; set; }
+
+        public string SecretKey { get; set; }
+
+        public int ExpirationTimeDays { get; set; }
+
+        public decimal AdditionalFee { get; set; }
+
+        public bool AdditionalFeePercentage { get; set; }
+
+        public bool EnableEpay { get; set; }
+
+        public bool EnableEasyPay { get; set; }
+
+        public bool EnableDirectCreditCard { get; set; }
+
+        public bool EnableCashOnDelivery { get; set; }
+    }
+}
---
+++ Plugins/Nop.Plugin.Payments.ePay/PaymentType.cs
@@ -0,0 +1,10 @@
+﻿namespace Nop.Plugin.Payments.ePay
+{
+    public enum PaymentType
+    {
+        Epay = 5,
+        EasyPay = 10,
+        DirectCreditCard = 15,
+        CashOnDelivery = 20
+    }
+}
